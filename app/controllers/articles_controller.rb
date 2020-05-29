14659de0abb88c334eae61c9b65c90ea8edfb764
Todd <todd.zaladonis@outlook.com>
2020-05-29 17:29:49 -0400

    Final I1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-‘include ArticlesHelper’
+include ArticlesHelper
 
     def index
         @articles = Article.all
@@ -14,7 +14,26 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         @article.save
+        flash.notice = "Article '#{@article.title}' Created!"
         redirect_to article_path(@article)
       
     end
+    def destroy
+        @article = Article.destroy(params[:id])
+        flash.notice = "Article '#{@article.title}' Deleted!"
+        redirect_to articles_path
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+      
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+
+        redirect_to article_path(@article)
+      end
+      
 end
